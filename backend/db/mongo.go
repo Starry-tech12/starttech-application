805e14dc3dbb539f29f3474ec7680d6590c26435
Esther Ayo Isaiah <esther.ayo.isaiah@example.com>
2026-05-25 17:29:57 +0100

    Fix CI/CD workflows (remove secrets)

---
+++ backend/db/mongo.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+
+func ConnectMongo() {
+	uri := os.Getenv("MONGO_URI")
+
+	if uri == "" {
+		log.Fatal("MONGO_URI not set")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("MongoDB connection failed:", err)
+	}
+
+	Client = client
+	log.Println("✅ MongoDB Connected Successfully")
+}
--- backend/main.go
+++ backend/main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+        "starttech/db"
 )
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Backend is running")
+func health(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "OK")
 }
 
 func main() {
-	http.HandleFunc("/health", healthHandler)
+	db.ConnectMongo()
+
+	http.HandleFunc("/health", health)
 
 	fmt.Println("Server running on :8080")
 	http.ListenAndServe(":8080", nil)
